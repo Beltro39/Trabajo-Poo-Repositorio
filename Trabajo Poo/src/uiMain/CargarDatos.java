90689756a9bd3fa182bdcc7b8b53dc9bc2bd665a
Beltro39 <beltran58d@gmail.com>
2020-03-06 12:01:49 -0500

    .

---
+++ Trabajo Poo/src/uiMain/CargarDatos.java
@@ -0,0 +1,10 @@
+package uiMain;
+import java.util.Vector;
+public class CargarDatos {
+    Vector<Double> pilaC= new Vector<Double>();
+    public void ejecutar(){
+    
+    pilaC.add()
+    //new Cliente("1001366260", "Sebastian", 19, "3007904147", 4, "Calle 13", new SeriviciosPublicos(new Vect))
+    }
+}
